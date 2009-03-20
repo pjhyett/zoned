17d284276dd979b9aee5cf5820c13545c3a166ec
Steve Iannopollo <steve@iannopollo.com>
2009-03-20 23:00:29 +0800

    Better specs for clarification
    Signed-off-by: PJ Hyett <pjhyett@gmail.com>

--- spec/zoned_spec.rb
+++ spec/zoned_spec.rb
@@ -1,11 +1,17 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+# Showing the timezone in this format so we realize that the time is not
+# changing timezones, but being offset in the same timezone just for looks
 Time::DATE_FORMATS[:full_date_time] = lambda do |time|
-  time.strftime("%B %d, %Y at %l:%M%p").sub(/AM/, 'am').sub(/PM/, 'pm').gsub(/\s\s+?/, ' ')
+  time.strftime("%B %d, %Y at %l:%M%p %Z").sub(/AM/, 'am').sub(/PM/, 'pm').gsub(/\s\s+?/, ' ')
 end
 
 describe "Zoned" do
   include Zoned
+  
+  before do
+    Time.stub!(:now).and_return(Time.parse('Fri, 20 Mar 2009 12:00:00 EDT'))
+  end
   
   def timezone_offset
     @timezone_offset
@@ -13,12 +19,23 @@ describe "Zoned" do
   
   it 'should take convert a date into the same date in a different timezone' do
     time = Time.parse("Fri, 20 Mar 2009 14:30:00 UTC +00:00")
-    time.to_s(:full_date_time).should == "March 20, 2009 at 2:30pm"
+    time.to_s(:full_date_time).should == "March 20, 2009 at 2:30pm UTC"
     
     @timezone_offset = -14400 # EDT timezone offset
-    z(time).to_s(:full_date_time).should == "March 20, 2009 at 10:30am"
+    z(time).to_s(:full_date_time).should == "March 20, 2009 at 10:30am UTC"
     
     @timezone_offset = -25200 # PDT timezone offset
-    z(time).to_s(:full_date_time).should == "March 20, 2009 at 7:30am"
+    z(time).to_s(:full_date_time).should == "March 20, 2009 at 7:30am UTC"
+  end
+  
+  it 'should convert a non-UTC time to the proper timezone' do
+    time = Time.parse("Fri, 20 Mar 2009 9:30:00 PDT")
+    time.to_s(:full_date_time).should == "March 20, 2009 at 12:30pm EDT"
+    
+    @timezone_offset = -14400 # EDT timezone offset
+    z(time).to_s(:full_date_time).should == "March 20, 2009 at 12:30pm UTC"
+    
+    @timezone_offset = -25200 # PDT timezone offset
+    z(time).to_s(:full_date_time).should == "March 20, 2009 at 9:30am UTC"
   end
 end
